d295db7391a72cf485c226ca61c1136aae2e670d
Maria Antici <maria.antici99@gmail.com>
2023-04-17 21:32:08 +0300

    Make Errands Lists reponsive

--- src/components/ErrandsListForMembers.tsx
+++ src/components/ErrandsListForMembers.tsx
@@ -5,6 +5,8 @@ import ErrandsList from "./ErrandsList";
 const members: { name: string }[] = [
     { name: 'Maria' },
     { name: 'Dragos' },
+    { name: 'Sara' },
+    { name: 'John' },
 ];
 
 // ErrandsListForMembers component
@@ -12,15 +14,18 @@ const ErrandsListForMembers: React.FC = () => {
     // Render the ErrandsListForMembers
     return (
         <>
-            <div className="flex justify-center gap-4 items-stretch">
+            <div className="flex flex-row flex-wrap lg:flex-nowrap justify-center items-stretch">
                 {members.map(({ name }, index) => (
                     <React.Fragment key={index}>
-                        <div className="card items-center justify-center bg-opacity-0 border-0 shadow-none">
+                        <div className="card items-center justify-center bg-opacity-0 border-0 shadow-none xs:mb-4 lg:mb-0">
                             <h2 className="mb-5 font-pacifico text-lg text-green-600">{name}</h2>
                             <ErrandsList />
                         </div>
                         {index !== members.length - 1 && (
-                            <div className="divider divider-vertical h-52"></div>
+                            <React.Fragment>
+                                <div className="divider divider-vertical h-0 lg:h-52 w-0 lg:w-full mx-0 lg:mx-4"></div>
+                                <div className="xs:hidden divider divider-horizontal w-40 mt-4 mb-4"></div>
+                            </React.Fragment>
                         )}
                     </React.Fragment>
                 ))}
