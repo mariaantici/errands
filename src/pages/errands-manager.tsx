f103a6c5eea0b3dd5d76e5aa0f66cf044f7685ba
Maria Antici <maria.antici99@gmail.com>
2023-04-17 17:31:20 +0300

    Add main page /errands-manager

--- src/pages/errands-manager.tsx
+++ src/pages/errands-manager.tsx
@@ -1,12 +1,30 @@
-import LogoutButton from "@/components/LogoutButton";
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
+import Tabs from "@/components/Tabs";
+import TabContent from "@/components/TabContent";
 
-export default function ErrandsManager() {
+// Sample data for the tabs
+const data: { [key: string]: string[] } = {
+    all: ['Item 1', 'Item 2', 'Item 3'],
+    household: ['Household 1', 'Household 2', 'Household 3'],
+    trip: ['Trip 1', 'Trip 2', 'Trip 3'],
+    workplace: ['Workplace 1', 'Workplace 2', 'Workplace 3'],
+};
+
+// ErrandsManager component
+const ErrandsManager: React.FC = () => {
+    const [activeTab, setActiveTab] = useState<string>('all');
+
+    // Render the ErrandsManager
     return (
         <>
-            <main>
-                <h1>Hello</h1>
-                <LogoutButton />
+            <Navbar />
+            <main className="max-w-5xl mx-auto">
+                <Tabs onTabChange={setActiveTab} activeTab={activeTab} />
+                <TabContent data={data[activeTab]} />
             </main>
         </>
     );
-}
+};
+
+export default ErrandsManager;
