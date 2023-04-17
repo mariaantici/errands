9589942936fd0a83698cc7c8b9a8ced4bc5ac748
Maria Antici <maria.antici99@gmail.com>
2023-04-17 17:31:36 +0300

    Create Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import LogoutButton from "@/components/auth/LogoutButton";
+
+// Navbar component
+const Navbar: React.FC = () => {
+    return (
+        <div className="h-[108px]">
+            <div className="navbar navbar-glass navbar-sticky max-w-[90%] lg:max-w-5xl mx-auto mt-5 rounded-xl">
+                <div className="navbar-start">
+                    <h1 className="font-pacifico text-2xl hidden lg:block">Tame the chaos, embrace </h1>
+                    <h1 className="font-pacifico text-2xl text-green-600 ml-1">Errands</h1>
+                </div>
+                <div className="navbar-end">
+                    <div className="avatar avatar-ring-success avatar-md">
+                        <div className="dropdown-container">
+                            <div className="dropdown">
+                                <label className="btn btn-ghost flex cursor-pointer px-0" tabIndex={0}>
+                                    <FontAwesomeIcon icon={faUser} className="text-green-600 sm:h-4 sm:w-4" />
+                                </label>
+                                <div className="dropdown-menu dropdown-menu-bottom-left">
+                                    <a tabIndex={-1} className="dropdown-item text-sm">Profile</a>
+                                    <a tabIndex={-1} className="dropdown-item text-sm">Settings</a>
+                                    <LogoutButton />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
