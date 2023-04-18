0e6b184851830584da3fb55c75e48b0880550e2d
Maria Antici <maria.antici99@gmail.com>
2023-04-18 18:15:55 +0300

    Update TabContent: use ReactDatePicker for mobile

--- src/components/TabContent.tsx
+++ src/components/TabContent.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddErrandButton from './AddErrandButton';
 import DatePicker from './DatePicker';
 import ErrandsListForMembers from './ErrandsListForMembers';
+import ReactDatePicker from './ReactDatePicker';
+import RecommendedErrands from './RecommendedErrands';
 
 // Define the props for the TabContent component
 interface TabContentProps {
@@ -10,12 +12,26 @@ interface TabContentProps {
 
 // TabContent component
 const TabContent: React.FC<TabContentProps> = ({ data }) => {
-    // Renter the TabContent
+    // State for managing selected date initialized with the current date
+    const [startDate, setStartDate] = useState<Date | null>(new Date());
+
+    // Render the TabContent
     return (
         <>
+            <div className="md:hidden flex justify-center">
+                <div className="flex items-baseline">
+                    <h2 className="whitespace-nowrap mr-2">Select date:</h2>
+                    <ReactDatePicker className="input mb-10 w-[108px]"
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                    />
+                </div>
+            </div>
+
             <DatePicker />
             <ErrandsListForMembers />
             <AddErrandButton />
+            <RecommendedErrands />
 
             {// Rendering sample data to show the Tabs work
             }
