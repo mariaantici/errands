3288c47c191c9fe5a50f29d02f0abcc5d542242b
Maria Antici <maria.antici99@gmail.com>
2023-04-18 19:44:34 +0300

    Fix ErrandsListForMembers:remove unnecessary space

--- src/components/ErrandsListForMembers.tsx
+++ src/components/ErrandsListForMembers.tsx
@@ -17,14 +17,14 @@ const ErrandsListForMembers: React.FC = () => {
             <div className="flex flex-row flex-wrap lg:flex-nowrap justify-center items-stretch">
                 {members.map(({ name }, index) => (
                     <React.Fragment key={index}>
-                        <div className="card items-center justify-center bg-opacity-0 border-0 shadow-none xs:mb-4 lg:mb-0">
+                        <div className="max-w-[200px] card items-center justify-center bg-opacity-0 border-0 shadow-none xxs:mb-4 lg:mb-0">
                             <h2 className="mb-5 font-pacifico text-lg text-green-600">{name}</h2>
                             <ErrandsList />
                         </div>
                         {index !== members.length - 1 && (
                             <React.Fragment>
                                 <div className="divider divider-vertical h-0 lg:h-52 w-0 lg:w-full mx-0 lg:mx-4"></div>
-                                <div className="xs:hidden divider divider-horizontal w-40 mt-4 mb-4"></div>
+                                <div className="xxs:hidden divider divider-horizontal w-[200px] mt-4 mb-4"></div>
                             </React.Fragment>
                         )}
                     </React.Fragment>
