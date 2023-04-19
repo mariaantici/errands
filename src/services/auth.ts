37d011d4216acf9be17e722c0bbbbd55b1eac713
Maria Antici <maria.antici99@gmail.com>
2023-04-19 22:46:29 +0300

    Refactor auth.ts: switch to try-catch for error handling

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,49 +3,83 @@ import { User } from '@supabase/supabase-js';
 
 // Register a new user with email and password
 export async function register(email: string, password: string): Promise<User | null> {
-    const { data, error } = await supabase.auth.signUp({ email, password });
+    try {
+        const { data, error } = await supabase.auth.signUp({ email, password });
 
-    if (error) {
+        if (error) {
+            throw error;
+        }
+
+        if (data?.session) {
+            supabase.auth.setSession(data.session);
+        }
+
+        return data?.user;
+
+    } catch (error) {
+        console.error('Error registering user:', error);
         throw error;
     }
-
-    if (data?.session) {
-        supabase.auth.setSession(data.session);
-    }
-
-    return data?.user;
 }
 
 // Log in an existing user with email and password
 export async function login(email: string, password: string): Promise<User | null> {
-    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-    if (error) {
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+        if (error) {
+            throw error;
+        }
+
+        return data?.user;
+
+    } catch (error) {
+        console.error('Error logging in user:', error);
         throw error;
     }
-    return data?.user;
 }
 
 // Log out the current user
 export async function logout(): Promise<void> {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
+    try {
+        const { error } = await supabase.auth.signOut();
+
+        if (error) {
+            throw error;
+        }
+
+    } catch (error) {
+        console.error('Error logging out user:', error);
         throw error;
     }
 }
 
 // Request a password reset for a user by their email
 export async function resetPassword(email: string): Promise<void> {
-    const { data, error } = await supabase.auth.resetPasswordForEmail(email);
-    if (error) {
+    try {
+        const { error } = await supabase.auth.resetPasswordForEmail(email);
+
+        if (error) {
+            throw error;
+        }
+
+    } catch (error) {
+        console.error('Error requesting password reset:', error);
         throw error;
     }
 }
 
 // Update the password for the current logged-in user
 export async function updatePassword(newPassword: string): Promise<void> {
-    const { error } = await supabase.auth.updateUser({ password: newPassword });
+    try {
+        const { error } = await supabase.auth.updateUser({ password: newPassword });
 
-    if (error) {
+        if (error) {
+            throw error;
+        }
+
+    } catch (error) {
+        console.error('Error updating password:', error);
         throw error;
     }
 }
