10dbe8088bfe363de0359ec64685ad3ff172f6f1
Maria Antici <maria.antici99@gmail.com>
2023-04-24 09:37:55 +0300

    Refactor: improve session management in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,39 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { useSessionContext } from '@supabase/auth-helpers-react';
+import { getSession } from '@/services/auth';
 
 // Home component
 const Home: React.FC = () => {
+  // Instantiate Next.js router
   const router = useRouter();
-  const { session } = useSessionContext();
 
-  // Redirect user based on authentication status
+  // useEffect to fetch the session and redirect the user
   useEffect(() => {
-    if (session) {
-      router.push('/errands-manager');
-    } else {
-      router.push('/authentication');
-    }
-  }, [router, session]);
+    // Function to fetch the session
+    async function fetchSession() {
+      try {
+        // Fetch the session
+        const session = await getSession();
 
-  // Render Loading while redirecting 
+        // Redirect user based on authentication status
+        if (session) {
+          // If the session exists, navigate to the errands-manager page
+          router.push('/errands-manager');
+        } else {
+          // If the session doesn't exist, navigate to the authentication page
+          router.push('/authentication');
+        }
+      } catch (error) {
+        // Show an alert if there is an error fetching the session
+        alert(error.message);
+      }
+    }
+
+    // Call fetchSession function
+    fetchSession();
+  }, [router]); // Run useEffect when the router instance changes
+
+  // Render Loading while redirecting
   return <div>Loading...</div>;
 };
 
