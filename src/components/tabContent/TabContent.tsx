b9b42b3cc2db28e287f1596f24737ac22d0f814c
Maria Antici <maria.antici99@gmail.com>
2023-04-24 17:24:16 +0300

    Add ShowSelectedDate and state for sharing the selected date across multiple components

--- src/components/tabContent/TabContent.tsx
+++ src/components/tabContent/TabContent.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DatePickerMobile from '@/components/datepickers/DatePickerMobile';
 import DatePicker from '@/components/datepickers/DatePicker';
+import ShowSelectedDate from './ShowSelectedDate';
 import ErrandsListForMembers from '@/components/tabContent/ErrandsListForMembers';
 import AddErrandButton from '@/components/tabContent/AddErrandButton';
 import RecommendedErrands from '@/components/tabContent/RecommendedErrands';
 
 // TabContent component
 const TabContent: React.FC<{ list: string }> = ({ list }) => {
+    // Set state for managing the selected date across the components
+    const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
     // Render the TabContent
     return (
         <>
-            <DatePickerMobile />
-            <DatePicker />
+            <DatePickerMobile selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+            <DatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+            <ShowSelectedDate date={selectedDate} />
             <ErrandsListForMembers />
             <AddErrandButton />
             <RecommendedErrands />
