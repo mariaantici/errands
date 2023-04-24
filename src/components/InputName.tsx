d461c97b3aab61fa9ed55f7c86eabbc9fbf4d70e
Maria Antici <maria.antici99@gmail.com>
2023-04-24 15:54:38 +0300

    Update error management using the Alert component

--- src/components/InputName.tsx
+++ src/components/InputName.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { updateUser } from "@/services/database/users";
+import { Alert } from '@/components/Alert';
 
 // InputName component
 const InputName: React.FC<{ isOpen?: boolean }> = ({ isOpen = false }) => {
     // State to handle the input value of the name field
     const [name, setName] = useState("");
     const [isModalOpen, setIsModalOpen] = useState<boolean>(isOpen);
+
+    // State to handle the alerts
+    const [alert, setAlert] = useState(null);
+    const [alertKey, setAlertKey] = useState(null);
 
     useEffect(() => {
         setIsModalOpen(isOpen);
@@ -16,16 +21,30 @@ const InputName: React.FC<{ isOpen?: boolean }> = ({ isOpen = false }) => {
         event.preventDefault();
         try {
             await updateUser(name);
-            console.log("Name updated successfully");
+            setAlert({ title: 'Success', message: 'Name updated successfully', type: 'success' });
+            setAlertKey(Date.now());
         } catch (error) {
-            console.error("Error updating name", error);
+            setAlert({ title: 'Error', message: error.message, type: 'error' });
+            setAlertKey(Date.now());
         }
-        setIsModalOpen(false);
+
+        // Close modal only if the name has a certain length
+        if (name.length > 1) {
+            setIsModalOpen(false);
+        }
     };
 
     // Render InputName
     return (
         <>
+            {alert && (
+                <Alert
+                    key={alertKey}
+                    title={alert.title}
+                    message={alert.message}
+                    type={alert.type}
+                />
+            )}
             {isModalOpen && (
                 <input className="modal-state" id="modalId" type="checkbox" defaultChecked />
             )}            <div className="modal">
@@ -38,11 +57,14 @@ const InputName: React.FC<{ isOpen?: boolean }> = ({ isOpen = false }) => {
                             Name
                         </label>
                         <input
-                            className="input mb-7 mt-[-5px]"
+                            className="input mt-[-5px]"
                             placeholder="Enter your name"
                             value={name}
                             onChange={(e) => setName(e.target.value)} // Update the name state when the input value changes
                         />
+                        <div className="h-7">
+                            {(name.length < 2 && name.length !== 0) && <p className="text-xs text-red-600">Name must have at least 2 characters</p>}
+                        </div>
                         <button className="btn btn-outline-success min-w-[200px] mx-auto" type="submit">
                             Save Name
                         </button>
