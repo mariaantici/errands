84fd8ebaf1b24b4828d44460e9b875782c97abcb
Maria Antici <maria.antici99@gmail.com>
2023-04-24 12:21:19 +0300

    Add InputName modal and user name check

--- src/pages/errands-manager.tsx
+++ src/pages/errands-manager.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Tabs from "@/components/Tabs";
 import TabContent from "@/components/tabContent/TabContent";
+import InputName from "@/components/InputName";
+import { getUser } from "@/services/database/users";
 
 // Sample data for the tabs
 const data: { [key: string]: string[] } = {
@@ -13,13 +15,38 @@ const data: { [key: string]: string[] } = {
 
 // ErrandsManager component
 const ErrandsManager: React.FC = () => {
+    // State to handle tabs
     const [activeTab, setActiveTab] = useState<string>('all');
+
+    // State to handle modal visibility
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    useEffect(() => {
+
+        // Check if a name exists for a user in the users table, if not it will create the user and open the modal
+        async function checkUserName() {
+            // Check if the user exists in the users table
+            try {
+                const user = await getUser();
+                console.log(user);
+
+                if (!user.name) {
+                    setIsOpen(true); // Open the modal for new users
+                }
+            } catch (error) {
+                alert(error.message);
+            }
+        };
+
+        checkUserName();
+    }, []);
 
     // Render the ErrandsManager
     return (
         <>
             <Navbar />
             <main className="max-w-5xl mx-auto">
+                <InputName isOpen={isOpen} />
                 <Tabs onTabChange={setActiveTab} activeTab={activeTab} />
                 <TabContent data={data[activeTab]} />
             </main>
