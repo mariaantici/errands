45750c2b6d8e0ff77134903fc7a4c5396d616d72
Maria Antici <maria.antici99@gmail.com>
2023-04-25 18:31:25 +0300

    Add createErrand method

--- src/services/database/errands.ts
+++ src/services/database/errands.ts
@@ -1,5 +1,19 @@
 import { supabase } from '@/utils/supabaseClient';
 import { Errand } from '@/services/types';
-import { getUserData } from '@/services/database/users';
 
+// Create new errand in errands table
+export async function createErrand(userId: string, list_name: string, name: string, date: Date): Promise<void> {
+    try {
+        const { error } = await supabase
+            .from('errands')
+            .insert({ user_id: userId, list_name: list_name, name: name, date: date });
 
+        if (error) {
+            throw error;
+        }
+
+    } catch (error) {
+        console.error('Error creating new errand', error);
+        throw error;
+    }
+}
