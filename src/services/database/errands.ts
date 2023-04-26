a8133905beecf4da53d4f04d9449f249c0130ac9
Maria Antici <maria.antici99@gmail.com>
2023-04-26 18:21:41 +0300

    Add fetching errands method for user using selected date

--- src/services/database/errands.ts
+++ src/services/database/errands.ts
@@ -17,3 +17,24 @@ export async function createErrand(userId: string, list_name: string, name: stri
         throw error;
     }
 }
+
+// Fetch all errands for user using the user's id and date
+export async function getErrands(userId: string, date: string): Promise<{}[] | null> {
+    try {
+        const { data, error } = await supabase
+            .from('errands')
+            .select('list_name, name, status')
+            .eq('user_id', userId)
+            .eq('date', date)
+
+        if (error) {
+            throw error;
+        }
+
+        return data;
+
+    } catch (error) {
+        console.error('Error fetching errands', error);
+        throw error;
+    }
+}
