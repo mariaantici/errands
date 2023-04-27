a29ee05327c4e924e245d21e3790d45146784ef0
Maria Antici <maria.antici99@gmail.com>
2023-04-27 11:42:47 +0300

    Add Spinner

---
+++ src/components/common/Spinner.tsx
@@ -0,0 +1,16 @@
+const Spinner: React.FC = () => {
+    return (
+        <div className="spinner-dot-circle spinner-success spinner-xl">
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+            <div className="spinner-dot"></div>
+        </div>
+    );
+}
+
+export default Spinner;
