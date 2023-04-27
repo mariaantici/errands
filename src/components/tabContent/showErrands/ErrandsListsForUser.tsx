974fc5d0047ee64b53b95d9faee24f4b62ab41d7
Maria Antici <maria.antici99@gmail.com>
2023-04-27 18:14:14 +0300

    Fix: adjust divider height according to the longest list

--- src/components/tabContent/showErrands/ErrandsListsForUser.tsx
+++ src/components/tabContent/showErrands/ErrandsListsForUser.tsx
@@ -82,11 +82,12 @@ const ErrandsListsForUser: React.FC<{ date: Date }> = ({ date }) => {
                     <p className="text-lg text-content3">No errands to be shown</p>
                 </div>
             ) : (
-                <div className="flex flex-row flex-nowrap justify-center items-start divide-x divide-content3">
+                <div className="flex flex-row flex-nowrap justify-center items-start">
                     <div className="flex flex-col items-center w-[400px]">
                         <h2 className="font-pacifico text-lg text-green-600 mb-3">done</h2>
                         <ErrandsList errands={list === "all" ? errandsData.filter((errand) => errand.status === true) : filteredErrands.filter((errand) => errand.status === true)} />
                     </div>
+                    <div className="divider divider-vertical h-auto self-stretch"></div>
                     <div className="flex flex-col items-center w-[400px]">
                         <h2 className="font-pacifico text-lg text-green-600 mb-3">undone</h2>
                         <ErrandsList errands={list === "all" ? errandsData.filter((errand) => errand.status === false) : filteredErrands.filter((errand) => errand.status === false)} />
