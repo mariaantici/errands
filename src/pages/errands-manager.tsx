20e07511822217eddc9779f532bd5d3da904d7f4
Maria Antici <maria.antici99@gmail.com>
2023-04-27 15:15:01 +0300

    Add loading state & ensure that the user is authenticated before displaying the ErrandsManager layout

--- src/pages/errands-manager.tsx
+++ src/pages/errands-manager.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import UserIdContext from "@/contexts/UserIdContext";
 import ActiveListContext from "@/contexts/ActiveListContext";
+import { getUser } from "@/services/database/users";
+import { useRouter } from 'next/router';
+import { Alert } from '@/components/Alert';
+import Spinner from '@/components/common/Spinner';
 import Navbar from "@/components/Navbar";
 import Tabs from "@/components/Tabs";
 import TabContent from "@/components/tabContent/TabContent";
 import InputName from "@/components/InputName";
-import { getUser } from "@/services/database/users";
-import { useRouter } from 'next/router';
-import { Alert } from '@/components/Alert';
 
 // ErrandsManager component
 const ErrandsManager: React.FC = () => {
@@ -24,11 +25,16 @@ const ErrandsManager: React.FC = () => {
     // State for saving the user's id
     const [userId, setUserId] = useState(null);
 
+    // State for managing loading state
+    const [isLoading, setIsLoading] = useState(true);
+
     const router = useRouter()
 
     useEffect(() => {
         // Check if a name exists for a user in the users table, if not it will create the user and open the modal
         async function checkUserName() {
+            setIsLoading(true);
+
             // Check if the user exists in the users table
             try {
                 const user = await getUser();
@@ -42,8 +48,12 @@ const ErrandsManager: React.FC = () => {
                 router.push('/authentication');
 
                 // Manage errors
-                setAlert({ title: 'Error', message: error.message, type: 'error' });
-                setAlertKey(Date.now());
+                if (error.message !== "invalid claim: missing sub claim") {
+                    setAlert({ title: 'Error', message: error.message, type: 'error' });
+                    setAlertKey(Date.now());
+                }
+            } finally {
+                setIsLoading(false);
             }
         };
 
@@ -61,16 +71,25 @@ const ErrandsManager: React.FC = () => {
                     type={alert.type}
                 />
             )}
-            <Navbar />
-            <main className="max-w-5xl mx-auto">
-                <InputName isOpen={isOpen} />
-                <Tabs onTabChange={setActiveTab} activeTab={activeTab} />
-                <UserIdContext.Provider value={userId}>
-                    <ActiveListContext.Provider value={activeTab}>
-                        <TabContent />
-                    </ActiveListContext.Provider>
-                </UserIdContext.Provider>
-            </main>
+            {isLoading || userId === null ? (
+                <div className="w-screen h-screen flex justify-center items-center">
+                    <Spinner />
+                </div>
+            ) : (
+                <>
+                    <Navbar />
+                    <main className="max-w-5xl mx-auto">
+                        <InputName isOpen={isOpen} />
+                        <Tabs onTabChange={setActiveTab} activeTab={activeTab} />
+                        <UserIdContext.Provider value={userId}>
+                            <ActiveListContext.Provider value={activeTab}>
+                                <TabContent />
+                            </ActiveListContext.Provider>
+                        </UserIdContext.Provider>
+                    </main>
+                </>
+            )}
+
         </>
     );
 };
