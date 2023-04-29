7895e0421db3d3f1b02c03824e2b4df2532d1837
Maria Antici <maria.antici99@gmail.com>
2023-04-29 12:20:00 +0300

    Check for existing lists before creating default lists for user

--- src/services/database/lists.ts
+++ src/services/database/lists.ts
@@ -3,27 +3,38 @@ import { List } from '@/services/types';
 
 // Create by default the 3 lists for a new user: Household, Trip & Workplace
 export async function createDefaultListsForUser(userId: string): Promise<void> {
+    // Create the default list of names for lists
     const defaultListNames = ['household', 'trip', 'workplace'];
+
     for (const listName of defaultListNames) {
-
-        // Create new list
+        // Check to see if the user is already a member of another list
         try {
-            const { error } = await supabase
+            const { data } = await supabase
                 .from('lists')
-                .insert([
-                    { list_name: listName, user_id: userId, is_owner: true },
-                ]);
+                .select('list_name')
+                .eq('list_name', listName)
+                .eq('user_id', userId);
 
-            if (error) {
-                throw error;
+            if (data.length === 0) {
+                // Create new list
+                try {
+                    const { error } = await supabase
+                        .from('lists')
+                        .insert([
+                            { list_name: listName, user_id: userId, is_owner: true },
+                        ]);
+
+                    if (error) {
+                        throw error;
+                    }
+                } catch (error) {
+                    console.error('Error creating default lists', error);
+                    throw error;
+                }
             }
-
-
         } catch (error) {
-            console.error('Error creating default lists', error);
+            console.error('The user already has this list', error);
             throw error;
         }
     }
 }
-
-
