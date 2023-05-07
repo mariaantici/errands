bf8a5d4cbc35cdb02dba6f9d8713a39c9fab622a
Maria Antici <maria.antici99@gmail.com>
2023-05-07 13:49:07 +0300

    Add component to manage deleting member

---
+++ src/components/tabContent/showErrands/DeleteMember.tsx
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import { createList, deleteList } from "@/services/database/lists";
+import ActiveListContext from "@/contexts/ActiveListContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMinus } from "@fortawesome/free-solid-svg-icons";
+
+// DeleteMember component
+const DeleteMember: React.FC<{ memberId: string, onMemberRemoved: () => void }> = ({ memberId, onMemberRemoved }) => {
+    // Get the active list from ActiveListContext
+    const list = useContext(ActiveListContext);
+
+    // Function to delete list for member
+    async function removeList() {
+        try {
+            await deleteList(list, memberId);
+
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    // Function to create new list for member
+    async function addList() {
+        try {
+            await createList(list, memberId);
+
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    // Function to handle removing a member and creating a new list for them
+    async function handleRemoveMember() {
+        try {
+            await removeList();
+            await addList();
+            onMemberRemoved();
+        } catch (error) {
+            console.error("Error removing member:", error);
+        }
+    }
+
+    // Render DeleteMember
+    return (
+        <div className="popover popover-hover">
+            <button className="popover-trigger" onClick={handleRemoveMember}>
+                <FontAwesomeIcon icon={faUserMinus} className="text-red-800 h-5 w-5 ml-2" />
+            </button>
+            <div className="popover-content popover-right w-min">
+                <div className="popover-arrow"></div>
+                <div className="text-sm">Remove</div>
+            </div>
+        </div>
+    );
+};
+
+export default DeleteMember;
