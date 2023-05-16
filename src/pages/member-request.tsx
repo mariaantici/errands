77bfaad042cd4f48ef190605788d6aeef7b05b3f
Maria Antici <maria.antici99@gmail.com>
2023-05-16 14:32:09 +0300

    Update member-request page to display all requests and manage them individually

--- src/pages/member-request.tsx
+++ src/pages/member-request.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { getUser } from "@/services/database/users";
-import { deleteRequest, fetchRequest } from "@/services/database/member-requests";
+import { getUser, getNameAndEmail } from "@/services/database/users";
+import { deleteRequest, getRequests } from "@/services/database/member-requests";
 import { updateListId } from "@/services/database/lists";
 import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { Alert } from "@/components/Alert";
 import Spinner from "@/components/common/Spinner";
 
@@ -14,8 +16,8 @@ const MemberRequest: React.FC = () => {
     // State to handle user email
     const [userEmail, setUserEmail] = useState(null);
 
-    // State to handle if there's a member request
-    const [hasRequest, setHasRequest] = useState(false);
+    // State to handle requests for user
+    const [requests, setRequests] = useState([]);
 
     // State for managing loading state
     const [isLoading, setIsLoading] = useState(true);
@@ -43,22 +45,22 @@ const MemberRequest: React.FC = () => {
         }
     };
 
-    // Update the user's list id if there's a request
-    const isRequest = async () => {
+    // Fetch Requests
+    const fetchRequests = async () => {
         try {
-            const request = await fetchRequest(userEmail);
+            const requests = await getRequests(userEmail);
 
-            if (request) {
-                setHasRequest(true);
+            // Check if requests is not an empty array
+            if (requests.length > 0) {
+                // iterate over each request and fetch user data
+                const requestsWithUserData = await Promise.all(requests.map(async (request) => {
+                    const senderData = await getNameAndEmail(request.sender_id);
+                    return { ...request, senderData };
+                }));
 
-                try {
-                    await updateListId(userId, request.list_id, request.list_name);
-                    await deleteRequest(request.id);
-
-                } catch (error) {
-                    setAlert({ title: 'Error', message: error.message, type: 'error' });
-                    setAlertKey(Date.now());
-                }
+                setRequests(requestsWithUserData);
+            } else {
+                setRequests([]);
             }
 
         } catch (error) {
@@ -75,22 +77,45 @@ const MemberRequest: React.FC = () => {
         fetchUser();
 
         if (userId) {
-            isRequest();
+            fetchRequests();
         }
 
     }, [userId]);
 
-    // Redirect user after a certain time when the loading state is false and userId is not null
-    useEffect(() => {
-        if (!isLoading && userId !== null) {
-            const timer = setTimeout(() => {
-                router.push('/errands-manager');
-            }, 5000); // = 5 seconds
+    // Function to accept member request
+    const acceptMemberRequest = async (listId: string, listName: string, requestId: string) => {
+        try {
+            await updateListId(userId, listId, listName);
+            await deleteRequest(requestId);
 
-            // Clean up the timer when the component is unmounted
-            return () => clearTimeout(timer);
+            setAlert({ title: 'Success', message: 'Invite has been accepted', type: 'success' });
+            setAlertKey(Date.now());
+
+            // Remove the request from the state
+            setRequests(requests.filter(request => request.id !== requestId));
+
+        } catch (error) {
+            setAlert({ title: 'Error', message: error.message, type: 'error' });
+            setAlertKey(Date.now());
         }
-    }, [isLoading, userId, router]);
+    }
+
+    // Function to decline member request
+    const deleteMemberRequest = async (requestId: string) => {
+        try {
+            await deleteRequest(requestId);
+
+            setAlert({ title: 'Success', message: 'Invite has been declined', type: 'success' });
+            setAlertKey(Date.now());
+
+            // Remove the request from the state
+            setRequests(requests.filter(request => request.id !== requestId));
+
+        } catch (error) {
+            setAlert({ title: 'Error', message: error.message, type: 'error' });
+            setAlertKey(Date.now());
+        }
+    }
 
     // Render the MemberRequest
     return (
@@ -109,26 +134,67 @@ const MemberRequest: React.FC = () => {
                 </div>
             ) : (
                 <div className="flex items-center justify-center min-h-screen">
-                    <div className="m-auto">
-                        <div className={`alert ${hasRequest ? "alert-success" : "alert-error"} max-w-xs sm:max-w-sm`}>
-                            {hasRequest ? (
-                                <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM18.58 32.58L11.4 25.4C10.62 24.62 10.62 23.36 11.4 22.58C12.18 21.8 13.44 21.8 14.22 22.58L20 28.34L33.76 14.58C34.54 13.8 35.8 13.8 36.58 14.58C37.36 15.36 37.36 16.62 36.58 17.4L21.4 32.58C20.64 33.36 19.36 33.36 18.58 32.58Z" fill="#00BA34" />
-                                </svg>
-                            ) : (
-                                <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M24 4C12.96 4 4 12.96 4 24C4 35.04 12.96 44 24 44C35.04 44 44 35.04 44 24C44 12.96 35.04 4 24 4ZM24 26C22.9 26 22 25.1 22 24V16C22 14.9 22.9 14 24 14C25.1 14 26 14.9 26 16V24C26 25.1 25.1 26 24 26ZM26 34H22V30H26V34Z" fill="#E92C2C" />
-                                </svg>
-                            )}
-                            <div className="flex w-full justify-between">
-                                <div className="flex flex-col">
-                                    <span>{hasRequest ? "Member request have been accepted" : "You have no member request"}</span>
-                                    <span className="text-content2 text-sm">we'll redirect you in a moment</span>
-                                </div>
+                    <div className="card m-auto max-w-[80%] md:max-w-[760px]">
+                        <div className="card-body">
+                            <div className="text-left mb-6">
+                                <h2 className="text-2xl font-pacifico mb-2">Your invites</h2>
+                                <p className="text-sm text-green-600 tracking-wide">they won't even know if you decline</p>
                             </div>
+                            <div className="flex flex-col">
+                                {requests.length === 0 ? (
+                                    <div className="flex justify-center items-center text-base">
+                                        You have no invites.
+                                    </div>
+                                ) : (
+                                    requests.map((request, index) => (
+                                        <>
+                                            <div key={index} className="flex flex-col divider:flex-row sm:items-center justify-between gap-4">
+                                                <div>
+                                                    <h6 className="text-base">
+                                                        <span className="font-pacifico">{request.senderData.name}</span>'s <span className="text-green-600">{request.list_name}</span> list invite
+                                                    </h6>
+                                                    <p className="text-sm text-content2">
+                                                        {request.senderData.email}
+                                                    </p>
+                                                </div>
+                                                <div className="flex gap-3">
+                                                    <button
+                                                        type="submit"
+                                                        className="btn btn-outline-success btn-md btn-block rounded-3xl"
+                                                        onClick={() => acceptMemberRequest(request.list_id, request.list_name, request.id)}
+                                                    >
+                                                        Accept
+                                                    </button>
+                                                    <button
+                                                        type="reset"
+                                                        className="btn btn-outline-error btn-md btn-block rounded-3xl"
+                                                        onClick={() => deleteMemberRequest(request.id)}
+                                                    >
+                                                        Decline
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            {index < requests.length - 1 && (
+                                                <div className="divider divider-horizontal w-full mx-auto"></div>
+                                            )}
+                                        </>
+                                    ))
+                                )}
+                            </div>
+                            <p className="text-xs text-content3 tracking-wide mt-6">
+                                {requests.length === 0
+                                    ? "No invites? No problem! Maybe it's time to be the inviter, not the invitee."
+                                    : "Joining another's list? Don't worry, your errands stay safe! But remember, your tasks will be visible to the other members."
+                                }
+                            </p>
+                            <a href="/errands-manager" className="flex items-center justify-end mt-4">
+                                <p className="text-xl font-pacifico">Continue to </p>
+                                <p className="text-xl ml-1 mr-2 font-pacifico text-green-600">Errands</p>
+                                <FontAwesomeIcon icon={faCircleArrowRight} className="h-6 text-green-600" />
+                            </a>
                         </div>
-                    </div>
-                </div>
+                    </div >
+                </div >
             )}
         </>
     );
