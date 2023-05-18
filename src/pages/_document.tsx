d7a0bee01ca1b7a3bac81910f0b6aa7d4440fcb9
Maria Antici <maria.antici99@gmail.com>
2023-05-18 17:13:58 +0300

    Add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Errands</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Pacifico&display=swap" rel="stylesheet" />
