bc9abff16af65192590b466210c811c0f39b6ef8
mariaantici <maria.antici99@gmail.com>
2023-07-18 09:58:08 +0300

    Fix datepicker by not opening the keyboard

--- src/components/errands-manager/tab-content/datepickers/ReactDatePicker.tsx
+++ src/components/errands-manager/tab-content/datepickers/ReactDatePicker.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import DatePicker from 'react-datepicker';
+import { forwardRef } from "react";
+import React from "react";
+import DatePicker from "react-datepicker";
 
 // Importing the styles for the react-datepicker
-import 'react-datepicker/dist/react-datepicker.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 // Defining the props for the ReactDatePicker component
 interface ReactDatePickerProps {
-    selected: Date | null;
-    onChange: (date: Date | null) => void;
-    className?: string;
-    placeholder?: string;
-    minDate?: Date | null;
+  selected: Date | null;
+  onChange: (date: Date | null) => void;
+  className?: string;
+  placeholder?: string;
+  minDate?: Date | null;
 }
 
+const DatepickerInput = ({ ...props }, ref: any) => (
+  <input type="text" {...props} readOnly />
+);
+
+export const CustomDatepickerInput = forwardRef(DatepickerInput);
+
 // ReactDatePicker component
-const ReactDatePicker: React.FC<ReactDatePickerProps> = ({ selected, onChange, className, placeholder, minDate = new Date() }) => {
-    //Render ReactDatePicker
-    return (
-        <DatePicker
-            className={className}
-            placeholderText={placeholder}
-            selected={selected}
-            onChange={onChange}
-            minDate={minDate}
-        />
-    );
+const ReactDatePicker: React.FC<ReactDatePickerProps> = ({
+  selected,
+  onChange,
+  className,
+  placeholder,
+  minDate = new Date(),
+}) => {
+  //Render ReactDatePicker
+  return (
+    <DatePicker
+      className={className}
+      placeholderText={placeholder}
+      selected={selected}
+      onChange={onChange}
+      minDate={minDate}
+      onFocus={(e) => (e.target.readOnly = true)}
+      customInput={<CustomDatepickerInput />}
+    />
+  );
 };
 
 export default ReactDatePicker;
